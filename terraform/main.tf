fd53a930ac62a5412ae86e154510e3be35630245
marciomfilho <107187625+marciomfilho@users.noreply.github.com>
2025-09-12 11:28:03 -0300

    main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0"  # Ajuste conforme sua versão instalada
+    }
+  }
+}
+
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
-  location = var.zone
+  location = var.zone       # use "us-central1-a" para zona ou "us-central1" para região
 
   initial_node_count = var.node_count
 
@@ -12,6 +21,6 @@ resource "google_container_cluster" "primary" {
   }
 
   workload_identity_config {
-  workload_pool = "${var.project_id}.svc.id.goog"
+    workload_pool = "${var.project_id}.svc.id.goog"
   }
 }
