d67b4118755851e03e5a396035bf93e9a6b04e28
marciomfilho <107187625+marciomfilho@users.noreply.github.com>
2025-09-17 11:21:37 -0300

    ajustes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,23 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  project     = var.GCP_PROJECT
+  region      = var.GCP_REGION
+  zone        = var.GCP_ZONE
+  credentials = file(var.GCP_CREDENTIALS_FILE)
+}
+
+variable "GCP_PROJECT" {
+  default = "careful-airfoil-470112-u0"
+}
+
+variable "GCP_REGION" {
+  default = "us-central1"
+}
+
+variable "GCP_ZONE" {
+  default = "us-central1-a"
+}
+
+variable "GCP_CREDENTIALS_FILE" {
+  description = "Caminho para o arquivo JSON de credenciais GCP"
+  type        = string
 }
